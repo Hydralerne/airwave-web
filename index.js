6f9b8eed74a3dd451e9b5113be152bb44e06b5cd
Hydra de lerne <abdelhamedmo59@gmail.com>
2024-11-05 17:03:42 +0200

    nothing

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ if (fs.existsSync(remotePath)) {
 app.set('trust proxy', true);
 
 let allowed = {}
-const isWeb = true
+
+const isWeb = false
 
 const accessControlBlock = (req, res, next) => {
     if (req.query.token == 'sjaiidlklqa' || Object.keys(allowed).includes(req.headers['x-real-ip'])) {
